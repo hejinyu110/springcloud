b3ad96eb3b5dc3c7911092813d4e4d6f107f7a11
hejinyu110 <930226173@qq.com>
2021-01-27 07:13:17 +0800

    fix:init

---
+++ springcloud-api/src/main/java/com/he/springcloud/pojo/Dept.java
@@ -0,0 +1,24 @@
+package com.he.springcloud.pojo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+//数据网络传输需要序列化
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)  //链式写法
+public class Dept implements Serializable {
+
+    private long deptno;
+    private String dname;
+    //这个数据是存在哪个库的  微服务架构 一个服务对应一个数据库
+    private String db_source;
+
+    public Dept (String dname){
+        this.dname = dname;
+    }
+    
+}
