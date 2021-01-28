0b17ad222de226fb656c84a209dbdc5d8914de33
hejinyu110 <930226173@qq.com>
2021-01-29 07:38:29 +0800

    customer

--- springcloud.provider-dept-8002/src/main/java/com/he/springcloud/DeptProvider_8002.java
+++ springcloud.provider-dept-8002/src/main/java/com/he/springcloud/DeptProvider_8002.java
@@ -2,8 +2,10 @@ package com.he.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient  //会自动在服务启动之后注册到 eureka 中
 public class DeptProvider_8002 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8002.class,args);
