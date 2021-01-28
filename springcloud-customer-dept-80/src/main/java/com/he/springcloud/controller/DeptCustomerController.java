bffbee9656003bbacbefe5af263c1ff1eedf0852
hejinyu <hejinyu@rongcloud.cn>
2021-01-28 14:01:06 +0800

    修改

---
+++ springcloud-customer-dept-80/src/main/java/com/he/springcloud/controller/DeptCustomerController.java
@@ -0,0 +1,10 @@
+package com.he.springcloud.controller;
+
+import jdk.nashorn.internal.ir.annotations.Reference;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DeptCustomerController {
+    //消费者 没有 service
+
+}
