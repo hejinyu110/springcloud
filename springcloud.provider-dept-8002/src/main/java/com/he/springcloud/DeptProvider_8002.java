5a4839539d815baf23eb3e32f1f3565ab9ed756f
hejinyu <hejinyu@rongcloud.cn>
2021-01-29 14:11:15 +0800

    注册中心集群

--- springcloud.provider-dept-8002/src/main/java/com/he/springcloud/DeptProvider_8002.java
+++ springcloud.provider-dept-8002/src/main/java/com/he/springcloud/DeptProvider_8002.java
@@ -2,10 +2,12 @@ package com.he.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient  //会自动在服务启动之后注册到 eureka 中
+@EnableDiscoveryClient //服务发现 可以获取到注册中心的服务
 public class DeptProvider_8002 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8002.class,args);
--- springcloud.provider-dept-8002/src/main/java/com/he/springcloud/controller/DeptController.java
+++ springcloud.provider-dept-8002/src/main/java/com/he/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.he.springcloud.controller;
 import com.he.springcloud.pojo.Dept;
 import com.he.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +14,9 @@ public class DeptController {
 
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient client;
 
 
     @RequestMapping("/dept/add")
@@ -28,5 +33,25 @@ public class DeptController {
     @GetMapping("/dept/list")
     public List<Dept> queryAll(){
         return deptService.queryAll();
+    }
+
+    //注册进来的微服务可以获取到一些消息
+    @GetMapping("/dept/discovery")
+    public Object discovery(){
+        //获取微服务信息
+        List<String> services = client.getServices();
+        System.out.println("discovery=>services"+services);
+        //得到一个具体的微服务信息
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-DEPT");
+        for (ServiceInstance instance : instances){
+            System.out.println(
+                    instance.getHost()+"\t"+
+                    instance.getPort()+"\t"+
+                    instance.getUri()+"\t"+
+                    instance.getServiceId()+"\t"
+            );
+        }
+        return this.client;
+
     }
 }
