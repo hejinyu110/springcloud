65ab3fa5171f00aad821b72fea5e28748424a675
hejinyu110 <930226173@qq.com>
2021-02-03 06:38:11 +0800

    hystrix 服务监控，必须要有一个服务降级接口才行，要先调用一次

--- springcloud.provider-dept-hystrix-8001/src/main/java/com/he/springcloud/DeptProvider_hystrix_8001.java
+++ springcloud.provider-dept-hystrix-8001/src/main/java/com/he/springcloud/DeptProvider_hystrix_8001.java
@@ -1,11 +1,14 @@
 package com.he.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient  //会自动在服务启动之后注册到 eureka 中
@@ -14,5 +17,13 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 public class DeptProvider_hystrix_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_hystrix_8001.class,args);
+    }
+
+    //增加一个 hystrix 监控的 地址 注册到bean 中
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        return registrationBean;
     }
 }
