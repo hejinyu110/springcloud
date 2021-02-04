017ad0ef8e15917c5c915c538b1f6cb45e04909e
hejinyu110 <930226173@qq.com>
2021-02-05 06:39:12 +0800

    springcloud-config-server-3355  config 客户端获取配置信息

--- springcloud-config-clinet-3355/src/main/java/com/he/springcloud/controller/ConfigClientController.java
+++ springcloud-config-clinet-3355/src/main/java/com/he/springcloud/controller/ConfigClientController.java
@@ -2,7 +2,9 @@ package com.he.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ConfigClientController {
 
     @Value("${spring.application.name}")
